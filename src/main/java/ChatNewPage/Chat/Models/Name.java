35b9dfbe6833201e4350bebcc2d975baa95afca4
Sergejus <Sergejus.balciunas@gmail.com>
2024-09-26 19:24:22 +0300

    service and controllers to control messages

---
+++ src/main/java/ChatNewPage/Chat/Models/Name.java
@@ -0,0 +1,16 @@
+package ChatNewPage.Chat.Models;
+
+public class Name {
+    private String name;
+
+    public Name() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
